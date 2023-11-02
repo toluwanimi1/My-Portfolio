8525ef295941020ac037b3fe96cbf17b8688da8b
Toluhomepc <toluwanimi2@gmail.com>
2023-11-02 10:50:56 -0400

    Navbar base started

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
-/*import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
     </div>
-  );
-}
-
-export default App;
-*/
-
-export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Toluwanimi Amudipe Portfolio
-    </h1>
   )
 }
+
+
+export default  App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import {FaBars, FaTimes} from 'react-icons/fa'
+import Logo from '../Assest/logo.png'
+const Navbar = () => {
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#1b082c] text-[#ffffff]'>
+    <div> <img src={Logo} alt="Logo image" style={{width: '70px'}}></img></div>    
+    {/*menu */}    
+    <div>
+            <ul className='flex'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        {/*Hameburger*/}
+        <div className='hidden'> <FaBars /></div>
+
+        {/* Mobile menu*/}
+        <div className='hidden'>
+        <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Experience</li>
+                <li>Projects</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
+
